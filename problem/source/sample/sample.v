c3b687a7f232596336920a763bbec82322223c9a
shuichi <shuichi.kozawa@gmail.com>
2023-02-19 22:32:45 +0900

    first commit

---
+++ problem/source/sample/sample.v
@@ -0,0 +1,20 @@
+module SAMPLE(
+    input CLK,
+    input RSTX,
+    input PSEL,
+    input PENABLE,
+    input [31:0] PADDR,
+    input PWRITE,
+    input [3:0] PSTRB,
+    input [31:0] PWDATA,
+    output PREADY,
+    output [31:0] PRDATA,
+);
+
+
+    reg aaa;
+
+
+
+endmodule
+
